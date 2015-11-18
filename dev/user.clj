56a939cd979480a51afe769f4dbe5aad69e9dfa4
Eric Fode <eric.fode@autodesk.com>
2015-11-18 10:16:35 -0800

    sigh

--- dev/user.clj
+++ dev/user.clj
@@ -8,7 +8,6 @@
 (def basic-auth ["u" "p"])
 
 (def service (map->SnowTable  { :base-url staging-domain :basic-auth basic-auth :snow-table "u_service"}))
-(def event   (map->SnowTable  { :base-url (:url config) :basic-auth ((juxt :username :password ) config) :snow-table "u_events" :default-limit 2}))
  
 (def router  (map->SnowTable  { :base-url staging-domain :basic-auth basic-auth
                               :snow-table "u_router"}))
